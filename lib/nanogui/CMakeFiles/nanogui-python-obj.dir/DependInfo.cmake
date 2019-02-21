f086ab8e20a277b03a4e2086ffe315229f8bbb0e
Songyi Huang <songyih1996@gmail.com>
2019-02-21 14:20:48 -0800

    Removing ignored files

---
+++ lib/nanogui/CMakeFiles/nanogui-python-obj.dir/DependInfo.cmake
@@ -0,0 +1,79 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/songyih/OneDrive/Documents/CMPT764/Assignment2/OBJViewer/lib/nanogui/ext/coro/coro.c" "/Users/songyih/OneDrive/Documents/CMPT764/Assignment2/OBJViewer/lib/nanogui/CMakeFiles/nanogui-python-obj.dir/ext/coro/coro.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "CORO_SJLJ"
+  "GLAD_GLAPI_EXPORT"
+  "NANOGUI_PYTHON"
+  "NANOGUI_SHARED"
+  "NVG_SHARED"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  "lib/nanogui/ext/eigen"
+  "lib/nanogui/ext/glfw/include"
+  "lib/nanogui/ext/nanovg/src"
+  "lib/nanogui/include"
+  "lib/nanogui"
+  "lib/nanogui/ext/coro"
+  "lib/nanogui/ext/pybind11/include"
+  "/Users/songyih/anaconda3/include/python3.6m"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/songyih/OneDrive/Documents/CMPT764/Assignment2/OBJViewer/lib/nanogui/python/basics.cpp" "/Users/songyih/OneDrive/Documents/CMPT764/Assignment2/OBJViewer/lib/nanogui/CMakeFiles/nanogui-python-obj.dir/python/basics.cpp.o"
+  "/Users/songyih/OneDrive/Documents/CMPT764/Assignment2/OBJViewer/lib/nanogui/python/button.cpp" "/Users/songyih/OneDrive/Documents/CMPT764/Assignment2/OBJViewer/lib/nanogui/CMakeFiles/nanogui-python-obj.dir/python/button.cpp.o"
+  "/Users/songyih/OneDrive/Documents/CMPT764/Assignment2/OBJViewer/lib/nanogui/python/constants_entypo.cpp" "/Users/songyih/OneDrive/Documents/CMPT764/Assignment2/OBJViewer/lib/nanogui/CMakeFiles/nanogui-python-obj.dir/python/constants_entypo.cpp.o"
+  "/Users/songyih/OneDrive/Documents/CMPT764/Assignment2/OBJViewer/lib/nanogui/python/constants_glfw.cpp" "/Users/songyih/OneDrive/Documents/CMPT764/Assignment2/OBJViewer/lib/nanogui/CMakeFiles/nanogui-python-obj.dir/python/constants_glfw.cpp.o"
+  "/Users/songyih/OneDrive/Documents/CMPT764/Assignment2/OBJViewer/lib/nanogui/python/eigen.cpp" "/Users/songyih/OneDrive/Documents/CMPT764/Assignment2/OBJViewer/lib/nanogui/CMakeFiles/nanogui-python-obj.dir/python/eigen.cpp.o"
+  "/Users/songyih/OneDrive/Documents/CMPT764/Assignment2/OBJViewer/lib/nanogui/python/formhelper.cpp" "/Users/songyih/OneDrive/Documents/CMPT764/Assignment2/OBJViewer/lib/nanogui/CMakeFiles/nanogui-python-obj.dir/python/formhelper.cpp.o"
+  "/Users/songyih/OneDrive/Documents/CMPT764/Assignment2/OBJViewer/lib/nanogui/python/glcanvas.cpp" "/Users/songyih/OneDrive/Documents/CMPT764/Assignment2/OBJViewer/lib/nanogui/CMakeFiles/nanogui-python-obj.dir/python/glcanvas.cpp.o"
+  "/Users/songyih/OneDrive/Documents/CMPT764/Assignment2/OBJViewer/lib/nanogui/python/glutil.cpp" "/Users/songyih/OneDrive/Documents/CMPT764/Assignment2/OBJViewer/lib/nanogui/CMakeFiles/nanogui-python-obj.dir/python/glutil.cpp.o"
+  "/Users/songyih/OneDrive/Documents/CMPT764/Assignment2/OBJViewer/lib/nanogui/python/layout.cpp" "/Users/songyih/OneDrive/Documents/CMPT764/Assignment2/OBJViewer/lib/nanogui/CMakeFiles/nanogui-python-obj.dir/python/layout.cpp.o"
+  "/Users/songyih/OneDrive/Documents/CMPT764/Assignment2/OBJViewer/lib/nanogui/python/main.cpp" "/Users/songyih/OneDrive/Documents/CMPT764/Assignment2/OBJViewer/lib/nanogui/CMakeFiles/nanogui-python-obj.dir/python/main.cpp.o"
+  "/Users/songyih/OneDrive/Documents/CMPT764/Assignment2/OBJViewer/lib/nanogui/python/misc.cpp" "/Users/songyih/OneDrive/Documents/CMPT764/Assignment2/OBJViewer/lib/nanogui/CMakeFiles/nanogui-python-obj.dir/python/misc.cpp.o"
+  "/Users/songyih/OneDrive/Documents/CMPT764/Assignment2/OBJViewer/lib/nanogui/python/nanovg.cpp" "/Users/songyih/OneDrive/Documents/CMPT764/Assignment2/OBJViewer/lib/nanogui/CMakeFiles/nanogui-python-obj.dir/python/nanovg.cpp.o"
+  "/Users/songyih/OneDrive/Documents/CMPT764/Assignment2/OBJViewer/lib/nanogui/python/tabs.cpp" "/Users/songyih/OneDrive/Documents/CMPT764/Assignment2/OBJViewer/lib/nanogui/CMakeFiles/nanogui-python-obj.dir/python/tabs.cpp.o"
+  "/Users/songyih/OneDrive/Documents/CMPT764/Assignment2/OBJViewer/lib/nanogui/python/textbox.cpp" "/Users/songyih/OneDrive/Documents/CMPT764/Assignment2/OBJViewer/lib/nanogui/CMakeFiles/nanogui-python-obj.dir/python/textbox.cpp.o"
+  "/Users/songyih/OneDrive/Documents/CMPT764/Assignment2/OBJViewer/lib/nanogui/python/widget.cpp" "/Users/songyih/OneDrive/Documents/CMPT764/Assignment2/OBJViewer/lib/nanogui/CMakeFiles/nanogui-python-obj.dir/python/widget.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "CORO_SJLJ"
+  "GLAD_GLAPI_EXPORT"
+  "NANOGUI_PYTHON"
+  "NANOGUI_SHARED"
+  "NVG_SHARED"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  "lib/nanogui/ext/eigen"
+  "lib/nanogui/ext/glfw/include"
+  "lib/nanogui/ext/nanovg/src"
+  "lib/nanogui/include"
+  "lib/nanogui"
+  "lib/nanogui/ext/coro"
+  "lib/nanogui/ext/pybind11/include"
+  "/Users/songyih/anaconda3/include/python3.6m"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
