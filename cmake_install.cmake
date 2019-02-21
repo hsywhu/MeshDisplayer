3c44cefaf2b80cb7a0b5a107362d17fd781a752f
Songyi Huang <songyih1996@gmail.com>
2019-02-21 14:00:48 -0800

    mac configure

--- cmake_install.cmake
+++ cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/songyih/CMPT764/Assignment1/nanogui_cmpt_464
+# Install script for directory: /Users/songyih/OneDrive/Documents/CMPT764/Assignment2/OBJViewer
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -27,11 +27,6 @@ if(NOT CMAKE_INSTALL_COMPONENT)
   endif()
 endif()
 
-# Install shared libraries without execute permission?
-if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
-  set(CMAKE_INSTALL_SO_NO_EXE "1")
-endif()
-
 # Is this installation the result of a crosscompile?
 if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
@@ -39,8 +34,8 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
-  include("/home/songyih/CMPT764/Assignment1/nanogui_cmpt_464/lib/nanogui/cmake_install.cmake")
-  include("/home/songyih/CMPT764/Assignment1/nanogui_cmpt_464/lib/embed-resource/cmake_install.cmake")
+  include("/Users/songyih/OneDrive/Documents/CMPT764/Assignment2/OBJViewer/lib/nanogui/cmake_install.cmake")
+  include("/Users/songyih/OneDrive/Documents/CMPT764/Assignment2/OBJViewer/lib/embed-resource/cmake_install.cmake")
 
 endif()
 
@@ -52,5 +47,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/songyih/CMPT764/Assignment1/nanogui_cmpt_464/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/songyih/OneDrive/Documents/CMPT764/Assignment2/OBJViewer/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
